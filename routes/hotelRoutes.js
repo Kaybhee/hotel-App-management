3513b430ed8a4cf303ccf5cacbc822712d086438
Kaybhee <abdulkabirbadru@gmail.com>
2025-04-20 02:29:25 +0100

    hotel routes

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import { userLogin, getAvailableRooms, getRoom, getAllRooms, changePrices, changeStatus } from '../userRole/user.js';
+import { createUser, createHotels, getAvailableRooms, getRoom, getAllRooms, changePrices, changeStatus } from '../userRole/user.js';
 
 // import
 
 const hotelRoutes = express.Router()
 
 
-hotelRoutes.post('/login', userLogin)
+hotelRoutes.post('/create-user', createUser)
+hotelRoutes.post('/create-hotels', createHotels)
 // get requests
 hotelRoutes.get('/rooms-available', getAvailableRooms)
 hotelRoutes.get('/rooms/:roomsId', getRoom) //after getting, purchase
@@ -16,7 +16,7 @@ hotelRoutes.get('/rooms', getAllRooms)
 hotelRoutes.put('/change-prices', changePrices)
 hotelRoutes.put('/change-status', changeStatus)
 // Delete 
-hotelRoutes.delete('/delete-room', deleteRoom)
+// hotelRoutes.delete('/delete-room', deleteRoom)
 // Create
 
 export default hotelRoutes
