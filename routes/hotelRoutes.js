78d924da66fc02dbddf6332c00e27dfa959f2c8b
Kaybhee <abdulkabirbadru@gmail.com>
2025-04-21 16:57:03 +0100

    hotel routes

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUser, createHotels, getAvailableRooms, getRoom, getAllRooms, changePrices, changeStatus } from '../userRole/user.js';
+import { createUser, createHotels, updateHotels, getHotels, getHotel, delHotels, getAvailableRooms, getRoom, getAllRooms, changePrices, changeStatus } from '../controller/user.js';
 
 // import
 
@@ -12,10 +12,15 @@ hotelRoutes.post('/create-hotels', createHotels)
 hotelRoutes.get('/rooms-available', getAvailableRooms)
 hotelRoutes.get('/rooms/:roomsId', getRoom) //after getting, purchase
 hotelRoutes.get('/rooms', getAllRooms)
+hotelRoutes.get('/:hotelId', getHotel)
+hotelRoutes.get('/', getHotels)
+
 // Put
+hotelRoutes.put('/update-hotels/:hotelId', updateHotels)
 hotelRoutes.put('/change-prices', changePrices)
 hotelRoutes.put('/change-status', changeStatus)
 // Delete 
+hotelRoutes.delete('/delete-hotel/:hotelId', delHotels)
 // hotelRoutes.delete('/delete-room', deleteRoom)
 // Create
 
