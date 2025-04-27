f5ea26e978b4174bc05834ab7089c7460575f42d
Kaybhee <abdulkabirbadru@gmail.com>
2025-04-27 23:12:42 +0100

    base routes for users, hotels

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { connectDB } from './config/db.js';
 import hotelRoutes from './routes/hotelRoutes.js'
 import authRoutes from './routes/authRoutes.js'
 import errCheck from './middlewares/errors/error.js';
+import userRoutes from './routes/userRoutes.js';
 import morgan from 'morgan'
 const PORT = 5000
 
@@ -17,7 +18,12 @@ app.use(morgan('dev'))
 // routes 
 app.use('/api/v1/hotels', hotelRoutes)
 app.use('/api/v1/auth', authRoutes)
-
+app.use('/api/v1/users', userRoutes)
+// app.use('/api/v1/users', (req, res, next) => {
+//     console.log('Request reached /api/v1/users');
+//     next();
+//   }, userRoutes);
+// console.log('user routes loaded')
 // Error middleware
 app.use(errCheck);
 // Connect to mongoDB
