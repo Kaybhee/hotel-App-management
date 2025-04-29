15e4254fd43644be7f9b492320132d8246b9b436
Kaybhee <abdulkabirbadru@gmail.com>
2025-04-29 18:11:03 +0100

    user authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,21 +2,18 @@ import express from 'express';
 import { adminOnly } from '../middlewares/Admin/isAdmin.js';
 import { protectAuth } from './../middlewares/auth/protectAuth.js';
 import { delUser, getUser, getUsers, updateUser, } from '../controller/users.js';
-// import { protectAuth } from '../middlewares/auth/protectAuth.js';
+import { protectAuth } from '../middlewares/auth/protectAuth.js';
 
 
 // import
 const userRoutes = express.Router()
 
 
-userRoutes.get('/user/', adminOnly, getUsers);
-userRoutes.get('/user/:userId', getUser);
-userRoutes.put('/user/update-user/:userId', protectAuth, updateUser)
-userRoutes.delete('/user/delete-user/:userId', adminOnly, delUser)
-// userRoutes.get('/', (req, res, next) => {
-//     console.log('GET /api/v1/users/ route hit');
-//     next();
-// }, getUsers)
+userRoutes.get('/', adminOnly, getUsers);
+userRoutes.get('/:userId', protectAuth, getUser);
+userRoutes.put('/update-user/:userId', protectAuth, updateUser)
+userRoutes.delete('/delete-user/:userId', adminOnly, delUser)
+
 
 // userRoutes.get('/',adminOnly, getUsers)
 
