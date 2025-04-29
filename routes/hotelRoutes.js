eb640b4994c41ba68ac48b43ee857d13cfdbee7b
Kaybhee <abdulkabirbadru@gmail.com>
2025-04-29 12:15:58 +0100

    hotel routes for update, and hotel rooms

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -1,25 +1,24 @@
 import express from 'express';
-import { createHotels, updateHotels, getHotels, getHotel, delHotels, getAvailableRooms, getRoom, getAllRooms, changePrices, changeStatus } from '../controller/hotel.js';
+import { createHotels, updateHotels, getHotels, getHotel, delHotels, getHotelRooms, getRoom, getAllRooms, changePrices, changeStatus } from '../controller/hotel.js';
+import { adminOnly } from '../middlewares/Admin/isAdmin.js';
 
 // import
 
 const hotelRoutes = express.Router()
 
 
-hotelRoutes.post('/create-hotels', createHotels)
+hotelRoutes.post('/create-hotels',  createHotels)
 // get requests
-hotelRoutes.get('/rooms-available', getAvailableRooms)
-hotelRoutes.get('/rooms/:roomsId', getRoom) //after getting, purchase
-hotelRoutes.get('/rooms', getAllRooms)
+hotelRoutes.get('/room/:hotelId', getHotelRooms)
 hotelRoutes.get('/:hotelId', getHotel)
 hotelRoutes.get('/', getHotels)
 
 // Put
-hotelRoutes.put('/update-hotels/:hotelId', updateHotels)
-hotelRoutes.put('/change-prices', changePrices)
-hotelRoutes.put('/change-status', changeStatus)
+hotelRoutes.put('/update-hotels/:hotelId', adminOnly, updateHotels)
+// hotelRoutes.put('/change-prices', changePrices)
+// hotelRoutes.put('/change-status', changeStatus)
 // Delete 
-hotelRoutes.delete('/delete-hotel/:hotelId', delHotels)
+hotelRoutes.delete('/delete-hotel/:hotelId', adminOnly, delHotels)
 // hotelRoutes.delete('/delete-room', deleteRoom)
 // Create
 
