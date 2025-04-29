c8bfb658745fde30ee559f174cb3d5b47ec66117
Kaybhee <abdulkabirbadru@gmail.com>
2025-04-29 18:21:01 +0100

    Removed redundancies

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -1,13 +1,11 @@
 import express from 'express';
-import { createHotels, updateHotels, getHotels, getHotel, delHotels, getHotelRooms, getRoom, getAllRooms, changePrices, changeStatus } from '../controller/hotel.js';
+import { createHotels, updateHotels, getHotels, getHotel, delHotels, getHotelRooms, } from '../controller/hotel.js';
 import { adminOnly } from '../middlewares/Admin/isAdmin.js';
-
-// import
 
 const hotelRoutes = express.Router()
 
 
-hotelRoutes.post('/create-hotels',  createHotels)
+hotelRoutes.post('/create-hotels',  adminOnly, createHotels)
 // get requests
 hotelRoutes.get('/room/:hotelId', getHotelRooms)
 hotelRoutes.get('/:hotelId', getHotel)
@@ -15,11 +13,8 @@ hotelRoutes.get('/', getHotels)
 
 // Put
 hotelRoutes.put('/update-hotels/:hotelId', adminOnly, updateHotels)
-// hotelRoutes.put('/change-prices', changePrices)
-// hotelRoutes.put('/change-status', changeStatus)
-// Delete 
+
 hotelRoutes.delete('/delete-hotel/:hotelId', adminOnly, delHotels)
-// hotelRoutes.delete('/delete-room', deleteRoom)
-// Create
+
 
 export default hotelRoutes
