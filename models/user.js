fd723187a2eb8cc76d37a1a668496de72e06a8cf
Kaybhee <abdulkabirbadru@gmail.com>
2025-04-30 10:01:55 +0100

    user schema

--- models/user.js
+++ models/user.js
@@ -25,6 +25,10 @@ const userSchema = mongoose.Schema({
           type: Boolean,
           default: false
         },
+        isVerified: {
+          type: Boolean,
+          default: false
+        }
       }, {timestamps: true}
 )
 const User = mongoose.model("User", userSchema);
