83b7e8a0e41008bed300fea7afc3f475100b56f3
Kaybhee <abdulkabirbadru@gmail.com>
2025-05-11 05:24:00 +0100

    added booking

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,28 @@
 import express from 'express';
 import { adminOnly } from '../middlewares/Admin/isAdmin.js';
 import { delUser, getUser, getUsers, updateUser, } from '../controller/users.js';
+import { bookRoom } from '../controller/booking.js';
 import { protectAuth } from '../middlewares/auth/protectAuth.js';
 
 
 // import
 const userRoutes = express.Router()
+const bookingRoutes = express.Router()
 
 
 userRoutes.get('/', adminOnly, getUsers);
 userRoutes.get('/:userId', protectAuth, getUser);
 userRoutes.put('/update-user/:userId', protectAuth, updateUser)
+
+// booking routes  
+bookingRoutes.post('/book-room', bookRoom)
+
+
 userRoutes.delete('/delete-user/:userId', adminOnly, delUser)
 
 
 // userRoutes.get('/',adminOnly, getUsers)
 
-
-export default userRoutes
+export { userRoutes, bookingRoutes }
 
 // Put
