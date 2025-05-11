4739d22e035ccfe001f01e6ed65f92ef970b42df
Kaybhee <abdulkabirbadru@gmail.com>
2025-05-11 05:20:45 +0100

    booking of rooms

---
+++ models/booking.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+const bookingSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
+    bookingDate: { type: Date, default: Date.now },
+})
+
+const Booking = mongoose.model('Booking', bookingSchema)
+export default Booking
