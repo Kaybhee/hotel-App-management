eb5c176c3c595ebf6e61a9292da6264f628f5565
Kaybhee <abdulkabirbadru@gmail.com>
2025-05-11 11:38:07 +0100

    added booking

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { adminOnly } from '../middlewares/Admin/isAdmin.js';
 import { protectAuth } from './../middlewares/auth/protectAuth.js';
 import { createRoom, getRoom, getRooms, updateRoom, delRoom, updateRoomAvailability } from '../controller/room.js';
+import { bookRoom } from '../controller/booking.js';
 // import { protectAuth } from '../middlewares/auth/protectAuth.js';
 
 
@@ -14,6 +15,7 @@ roomRoutes.post('/create-room/:hotelId', adminOnly, createRoom);
 roomRoutes.get('/', getRooms);
 roomRoutes.get('/:roomId', getRoom);
 roomRoutes.put('/availability/:roomNumberId', updateRoomAvailability)
+roomRoutes.post('/book-room', bookRoom)
 roomRoutes.put('/update-room/:roomId', adminOnly, updateRoom)
 roomRoutes.delete('/delete-room/:roomId/:hotelId', adminOnly, delRoom)
 
