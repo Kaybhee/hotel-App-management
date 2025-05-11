3bf042be75f5ba9355a396c3aa13f922e078e6ca
Kaybhee <abdulkabirbadru@gmail.com>
2025-05-11 11:39:01 +0100

    Deleting expired date schedule

--- jobs/cleanDates.js
+++ jobs/cleanDates.js
@@ -1,14 +1,23 @@
 import cron from 'node-cron';
 import Room from '../models/rooms.js';
 
-cron.schedule('0 0 * * *', async (next) => {
+cron.schedule('* * * * *', async (next) => {
     try {
+        console.log('Running cron job to clean expired dates...');
         const rooms = await Room.find();
         for ( const room of rooms) {
+            room.roomNumbers = room.roomNumbers.filter((roomNumber) => {
+                if (!roomNumber.number) {
+                    console.warn(`Skipping roomNumber in room ${room._id} due to missing number`)
+                    return false;
+                }
+                // keep valid number
+                return true;
+            })
             room.roomNumbers.forEach((roomNumber) => {
                 const originalDates = [...roomNumber.unavailableDates];
                 roomNumber.unavailableDates = roomNumber.unavailableDates.filter((date) => new Date(date) >= new Date())
-                console.log(`Room $room._id}, Room number ${roomNumber.number}:`) 
+                console.log(`Room ${room._id}, Room number ${roomNumber.number}:`) 
                 console.log(`Original Dates: ${originalDates}`);
                 console.log(`Updated Dates: ${roomNumber.unavailableDates}`);
             })
