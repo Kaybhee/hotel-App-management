41e8dfd279b634cbf2652bbd1763a22ba6779613
Kaybhee <abdulkabirbadru@gmail.com>
2025-06-19 07:57:02 +0100

    removed the file

--- encrypt.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt';
-
-
-const encryptPass = async(password) => {
-    const salt = await bcrypt.genSalt(10)
-    return await bcrypt.hash(password, salt)
-}
-
-export default encryptPass
