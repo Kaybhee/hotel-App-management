1ff550bd94e99d787a3d3ff61072c417b4673c36
Kaybhee <abdulkabirbadru@gmail.com>
2026-01-12 09:31:47 +0100

    implementing the rateLimiter middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { register, userLogin, verifyUserRegistration, adminLogin, resendUserLink } from '../controller/auth.js';
 import { adminOnly } from '../middlewares/Admin/isAdmin.js';
-
+import { rateLimiter } from '../middlewares/rateLimit/rateLimiter.js'
 
 // routes
 const authRoutes = express.Router()
 
 // post & patch method for users
 authRoutes.post('/create-user', register)
-authRoutes.post('/resend-user-otp', resendUserLink)
+authRoutes.post('/resend-link', rateLimiter, resendUserLink)
 // authRoutes.patch('/verify-user-registration', verifyUserRegistration)
 authRoutes.get('/verify-email', verifyUserRegistration)
 authRoutes.post('/login', userLogin) // Login user
